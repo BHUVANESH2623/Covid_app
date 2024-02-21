8e8bf6d613d6120c565ca65c44a9c89f585db370
BHUVANESH G <bhuvaneshcse03@gmail.com>
2024-02-21 17:53:17 +0530

    first commit

---
+++ client/src/home/Home.jsx
@@ -0,0 +1,14 @@
+import './home.scss';
+
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to the COVID Vaccination Slot Management System</h1>
+      <p>
+        This application is designed to help manage COVID vaccination slots for various centers.
+        Users can add available slots, and new users can register or log in to book their vaccination slots.
+      </p>
+    </div>
+  );
+};
---
+++ client/src/home/Navbar.jsx
@@ -0,0 +1,40 @@
+import Button from "@mui/material/Button";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
+export const Navbar = () => {
+  const { id } = useContext(UserContext);
+  return (
+    <div className="navbar">
+      <div className="left">
+        <h2>Covid App</h2>
+      </div>
+      <div className="right">
+        {id === 1 && (
+          <Button className="btn">
+            <Link className="link" to={"/addcenter"}>
+              Add Centers
+            </Link>
+          </Button>
+        )}
+        <Button className="btn">
+          <Link className="link" to={"/bookslot"}>
+            Book Slot
+          </Link>
+        </Button>
+        <Button className="btn">
+          <Link className="link" to={"/login"}>
+            Login
+          </Link>
+        </Button>
+        <Button className="btn">
+          <Link className="link" to={"/register"}>
+            Register
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
