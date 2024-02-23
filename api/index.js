99918f6b96244016f53db662a4d0a1e16dbc8c4e
BHUVANESH G <bhuvaneshcse03@gmail.com>
2024-02-23 18:30:41 +0530

    five commit

--- api/index.js
+++ api/index.js
@@ -6,12 +6,16 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-// app.use((req,res,next)=>{
-//     res.header("Access-Control-Allow-Credentials",true);
-//     next();
-// })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://covid-app-frontend.onrender.com",
+  })
+);
 app.use(cookieParser());
 
 app.use("/log", userRoutes);
