112f675b71072feb3f2542959e71912fe642a8ec
BHUVANESH G <bhuvaneshcse03@gmail.com>
2024-02-23 17:54:09 +0530

    second commit

--- api/db.js
+++ api/db.js
@@ -1,10 +1,19 @@
-import mysql from 'mysql';
+import mysql from "mysql2";
 
-const db=mysql.createConnection({
-    user:"root",
-    host:"localhost",
-    password:'Bhuvi@123',
-    database:'covidapp'
+// const db = mysql.createConnection({
+//   user: "avnadmin",
+//   host: "mysql-26437d1a-projectdev.a.aivencloud.com",
+//   password: "AVNS_zDt2MyQWGSd7K8FX0VF",
+//   database: "covidapp",
+// });
+
+const db = mysql.createConnection(
+  `mysql://avnadmin:AVNS_zDt2MyQWGSd7K8FX0VF@mysql-26437d1a-projectdev.a.aivencloud.com:20951/covidapp`
+);
+
+db.connect((err) => {
+  if (err) console.log(err);
+  else console.log("database connected");
 });
 
 export default db;
