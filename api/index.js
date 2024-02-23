9ff09757ecc85f1524c9018fe939d0f16bbfe813
BHUVANESH G <bhuvaneshcse03@gmail.com>
2024-02-23 18:13:21 +0530

    fourth commit

--- api/index.js
+++ api/index.js
@@ -1,28 +1,23 @@
+import express from "express";
+import userRoutes from "./routes/user.js";
+import addRoutes from "./routes/add.js";
+import bookRoutes from "./routes/book.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import express from 'express';
-import userRoutes from './routes/user.js';
-import addRoutes from './routes/add.js';
-import bookRoutes from './routes/book.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-
-
-const app=express();
-app.use((req,res,next)=>{
-    res.header("Access-Control-Allow-Credentials",true);
-    next();
-})
+const app = express();
+// app.use((req,res,next)=>{
+//     res.header("Access-Control-Allow-Credentials",true);
+//     next();
+// })
 app.use(express.json());
-app.use(cors({
-    origin:"http://localhost:3000"
-}));
+app.use(cors());
 app.use(cookieParser());
 
-app.use('/log',userRoutes);
-app.use('/add',addRoutes);
-app.use('/book',bookRoutes);
+app.use("/log", userRoutes);
+app.use("/add", addRoutes);
+app.use("/book", bookRoutes);
 
-
-app.listen(8080,()=>{
-    console.log("server connected");
-})
+app.listen(8080, () => {
+  console.log("server connected");
+});
